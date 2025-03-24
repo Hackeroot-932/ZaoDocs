e48506bad5a8dde8f5eb84f590ab2839e14138f6
HackeRoot <zsec932@proton.me>
2025-03-24 18:27:44 +0100

    Adding Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { RootProvider } from "fumadocs-ui/provider";
 import { Inter } from "next/font/google";
 import type { ReactNode } from "react";
 import { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({
   subsets: ["latin"],
